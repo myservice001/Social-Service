0b4d5f2875cf3a0572343e4437f2b0d2e35b2435
myservice001 <myservice4you123@gmail.com>
2025-07-24 18:56:55 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Replace the below config with your Firebase project config
+const firebaseConfig = {
+  apiKey: "AIzaSyAKdvXVd8XgtmNDL_UH5vB9f6p6y9-QlAQ",
+  authDomain: "social-service-3877a.firebaseapp.com",
+  projectId: "social-service-3877a",
+  storageBucket: "social-service-3877a.firebasestorage.app",
+  messagingSenderId: "267130398312",
+  appId: "1:267130398312:web:4a82d0848ed9e0958dfddd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);

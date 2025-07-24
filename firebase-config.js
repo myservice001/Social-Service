b8484039f63fc829b6e0d0494a1bc0cade08d1d3
myservice001 <myservice4you123@gmail.com>
2025-07-24 20:05:41 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-// Replace the below config with your Firebase project config
+// firebase-config.js
+
+// Import Firebase (compat version, since you're using script tag style)
 const firebaseConfig = {
   apiKey: "AIzaSyAKdvXVd8XgtmNDL_UH5vB9f6p6y9-QlAQ",
   authDomain: "social-service-3877a.firebaseapp.com",
   projectId: "social-service-3877a",
-  storageBucket: "social-service-3877a.firebasestorage.app",
-  messagingSenderId: "267130398312",
-  appId: "1:267130398312:web:4a82d0848ed9e0958dfddd"
+  appId: "1:267130398312:web:4a82d0848ed9e0958dfddd",
 };
 
 // Initialize Firebase
